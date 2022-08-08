257eae6946ea8b8a3685ede3c10c37e9db705279
Ye Moe <yeahmoeee@Yes-MacBook-Pro.local>
2022-08-08 09:18:05 +0630

    Updated

--- main.py
+++ main.py
@@ -12,21 +12,19 @@ def main():
         if board.check_game() != 2:
             break
         if (i + player) % 2 == 0:
-            count = 0
             board.ai_turn()
-            print("Count:", count)
         else:
-            board.drawboard()
+            board.draw_board()
             board.human_turn()
     result = board.check_game()
     if result == 0:
-        board.drawboard()
+        board.draw_board()
         print("Draw!")
     elif result == 1:
-        board.drawboard()
+        board.draw_board()
         print("AI(X) Wins! Human(O) Loses!")
     elif result == -1:
-        board.drawboard()
+        board.draw_board()
         print("Human(O) Wins! AI(X) Loses!")
 
 main()
--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,7 @@ class tictactoe:
         self.board = board
         self.winning_pos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-    def drawboard(self):
+    def draw_board(self):
         print("Current state: \n\n")
         for i in range(0,9):
             if self.board[i] == 0:
